fa3f376251d615efde7c7de4692514dd86a1f596
Stephen Just <sajust@ualberta.ca>
2013-11-17 14:05:26 -0700

    added database populate function from sample

--- src/DbHelper.java
+++ src/DbHelper.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Random;
 
 import com.sleepycat.db.Database;
 import com.sleepycat.db.DatabaseConfig;
+import com.sleepycat.db.DatabaseEntry;
 import com.sleepycat.db.DatabaseException;
 import com.sleepycat.db.DatabaseType;
 
@@ -36,6 +38,61 @@ public class DbHelper {
 			System.err.println("Failed to create database file");
 		}
 		return null;
+	}
+
+	/*
+	 *  To populate the given table with nrecs records
+	 */
+	static void populateTable(Database my_table, int nrecs ) {
+		int range;
+		DatabaseEntry kdbt, ddbt;
+		String s;
+
+		/*  
+		 *  generate a random string with the length between 64 and 127,
+		 *  inclusive.
+		 *
+		 *  Seed the random number once and once only.
+		 */
+		Random random = new Random(1000000);
+
+		try {
+			for (int i = 0; i < nrecs; i++) {
+
+				/* to generate a key string */
+				range = 64 + random.nextInt( 64 );
+				s = "";
+				for ( int j = 0; j < range; j++ ) 
+					s+=(new Character((char)(97+random.nextInt(26)))).toString();
+
+				/* to create a DBT for key */
+				kdbt = new DatabaseEntry(s.getBytes());
+				kdbt.setSize(s.length()); 
+
+				// to print out the key/data pair
+				// System.out.println(s);	
+
+				/* to generate a data string */
+				range = 64 + random.nextInt( 64 );
+				s = "";
+				for ( int j = 0; j < range; j++ ) 
+					s+=(new Character((char)(97+random.nextInt(26)))).toString();
+				// to print out the key/data pair
+				// System.out.println(s);	
+				// System.out.println("");
+
+				/* to create a DBT for data */
+				ddbt = new DatabaseEntry(s.getBytes());
+				ddbt.setSize(s.length()); 
+
+				/* to insert the key/data pair into the database */
+				my_table.putNoOverwrite(null, kdbt, ddbt);
+			}
+		}
+		catch (DatabaseException dbe) {
+			System.err.println("Populate the table: "+dbe.toString());
+			System.exit(1);
+		}
 	}
 
 }
