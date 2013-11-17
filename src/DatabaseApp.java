f46cf048d9c458617a38471a78a98f0f6f10fac6
Stephen Just <sajust@ualberta.ca>
2013-11-17 12:56:08 -0700

    Add methods to make main class non-static :)

--- src/DatabaseApp.java
+++ src/DatabaseApp.java
@@ -2,8 +2,23 @@
 public class DatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DatabaseApp app = new DatabaseApp();
+		try {
+			app.setup();
+			app.run();
+		} finally {
+			app.cleanup();
+		}
+	}
+	
+	public void setup() {
+		
+	}
+	public void run() {
+		
+	}
+	public void cleanup() {
+		
 	}
 
 }
