acb67bc04d3b206259ae399f0c7711d2b19c16b2
Stefan Martynkiw <linster247@gmail.com>
2013-11-17 13:17:54 -0700

    Removed menu system, added parsing of commandline arguments

--- src/DatabaseApp.java
+++ src/DatabaseApp.java
@@ -4,54 +4,28 @@ import java.io.InputStreamReader;
 
 
 public class DatabaseApp {
-
+	Integer mode = 0;
 	public static void main(String[] args) {
 		DatabaseApp app = new DatabaseApp();
 		try {
-			app.setup();
+			app.setup(args);
 			app.run();
 		} finally {
 			app.cleanup();
 		}
 	}
 	
-	public void setup() {
+	public void setup(String[] args) {
+		/** Select the appropriate mode based on commandline arguments
+		 */
+		
+		if (args[0].equals("btree")) this.mode = 1;
+		if (args[0].equals("hash")) this.mode = 2;
+		if (args[0].equals("indexfile")) this.mode = 3;
 		
 	}
 	public void run() {
-		/** Display the main menu, prompt the user for which db type is being used
-		 * 
-		 */
-		while (true) {
-			System.out.println("CMPUT 291 Project 2");
-			System.out.println("-------------------");
-			System.out.println("Select Test Option");
-			System.out.println("1) btree");
-			System.out.println("2) hash");
-			System.out.println("3) indexfile");
-			System.out.println("0) Exit");
-			
-			BufferedReader br = null;
-			br = new BufferedReader(new InputStreamReader(System.in));
-			Integer inputnumber = 0;
-			try {
-				String input = br.readLine();
-				inputnumber = Integer.parseInt(input);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NumberFormatException e) {
-				System.out.println("Invalid Entry, please try again");
-				continue;
-			}
-			
-			switch(inputnumber) {
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			}
-			
-		}
+		
 		
 	}
 	public void cleanup() {
