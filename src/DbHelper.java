6a9a400fdb2b491a02e26931cff348281ee7941b
Stefan Martynkiw <linster247@gmail.com>
2013-11-23 14:32:36 -0700

    Latest Stuff for get by key.

--- src/DbHelper.java
+++ src/DbHelper.java
@@ -1,11 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
+import com.sleepycat.db.Cursor;
 import com.sleepycat.db.Database;
 import com.sleepycat.db.DatabaseConfig;
 import com.sleepycat.db.DatabaseEntry;
 import com.sleepycat.db.DatabaseException;
 import com.sleepycat.db.DatabaseType;
+import com.sleepycat.db.LockMode;
+import com.sleepycat.db.OperationStatus;
 
 public class DbHelper {
 
@@ -87,6 +94,86 @@ public class DbHelper {
 			System.err.println("Populate the table: "+dbe.toString());
 			System.exit(1);
 		}
+	}
+	
+	
+	
+	public void getByKey(Database db, String answerFileName, String skey) {
+		// Search the DB by key.
+		// In the answers file, must have following 3 line format:
+		//		KeyString
+		//		DataString
+		//		EmptyString
+		
+		// Print to the screen:
+		//		Number of records retrieved
+		//		Execution time in Microseconds.
+		
+		// Assume that the class variable for db is called db.
+		String KeyString = skey;
+		String DataString;
+		String EmptyString = "";
+		
+		File file = new File(answerFileName);
+		 
+		
+		
+		
+		try {
+			
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			//Write to file using bw.write(String);
+			
+			OperationStatus oprStatus; 
+			DatabaseEntry dbKey = new DatabaseEntry(skey.getBytes());
+			DatabaseEntry dbData = new DatabaseEntry();
+			oprStatus = db.get(null, dbKey , new DatabaseEntry(), LockMode.DEFAULT );
+			
+			 if (oprStatus == OperationStatus.KEYEMPTY) {
+					System.out.println("Key was empty");
+					bw.close();
+					return;
+			 } else if (oprStatus == OperationStatus.NOTFOUND) {
+					System.out.println("No data found");
+					bw.close();
+					return;
+			 } else if (oprStatus != OperationStatus.SUCCESS) {
+					System.out.println("General failure to succeed");
+					bw.close();
+					return;
+			 } else {
+				 //Success
+				 //Make a cursor to traverse through data.
+				 //http://docs.oracle.com/cd/E17277_02/html/GettingStartedGuide/Positioning.html
+				 
+				 Cursor cursor = db.openCursor(null,  null);
+				 DatabaseEntry foundData = new DatabaseEntry();
+				 
+				 
+				 
+			 }
+			 
+			 
+			 
+		
+			 bw.close();
+		
+		} catch (DatabaseException e) {
+			System.out.println("Database Exception in getByKey");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO Exception. Something is wrong with file creation");
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
 	}
 
 }
