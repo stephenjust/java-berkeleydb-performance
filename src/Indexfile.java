1f975abbf16e5007a4c457ec2eddd2e2f62cd299
Stefan Martynkiw <linster247@gmail.com>
2013-11-26 18:27:05 -0700

    New class to deal with IndexFile databases

---
+++ src/Indexfile.java
@@ -0,0 +1,40 @@
+
+public class Indexfile extends BaseDb implements ISearch {
+
+	public Indexfile(String dbPath) {
+		super(dbPath);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void populateTable(int nrecs) {
+	
+	}
+
+	@Override
+	public void getByKey(String skey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getByValue(String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void retrieveRange(String startKey, String endKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
