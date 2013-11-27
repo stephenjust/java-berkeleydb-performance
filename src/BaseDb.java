8d1ada78db578d93009000cadb6a84fdfb5d2415
Stephen Just <sajust@ualberta.ca>
2013-11-26 18:03:29 -0700

    Remove more list stuff

--- src/BaseDb.java
+++ src/BaseDb.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import com.sleepycat.db.Cursor;
@@ -174,7 +172,6 @@ public abstract class BaseDb implements ISearch {
 
 	@Override
 	public void retrieveRange(String startKey, String endKey) {
-			List<String> list = new ArrayList<String>();
 			FileOutputStream fos = null;
 			
 			long sTime = System.nanoTime();
