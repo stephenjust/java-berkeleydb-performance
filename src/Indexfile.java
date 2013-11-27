4f22806ad4ed310ee8b9feb56d28bcbb1d9e2b0d
Stefan Martynkiw <linster247@gmail.com>
2013-11-26 18:34:16 -0700

    Added constructor

--- src/Indexfile.java
+++ src/Indexfile.java
@@ -1,9 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.sleepycat.db.Database;
+import com.sleepycat.db.DatabaseConfig;
+import com.sleepycat.db.DatabaseException;
+import com.sleepycat.db.DatabaseType;
+
 
 public class Indexfile extends BaseDb implements ISearch {
 
+	public Database table;
+	public Database index;
+	
+	
 	public Indexfile(String dbPath) {
 		super(dbPath);
-		// TODO Auto-generated constructor stub
+		// database configuration
+		DatabaseConfig dbConfig = new DatabaseConfig();
+
+		dbConfig.setErrorStream(System.err);
+		dbConfig.setType(DatabaseType.BTREE);
+		dbConfig.setAllowCreate(true);
+
+		// database
+		try {
+			table = new Database(dbPath + File.pathSeparator + "table.db", null, dbConfig);
+			index = new Database(dbPath + File.pathSeparator + "index.db", null, dbConfig);
+		} catch (FileNotFoundException e) {
+			System.err.println("Database file not found");
+		} catch (DatabaseException e) {
+			System.err.println("Failed to open database");
+		}
 	}
 	
 	@Override
