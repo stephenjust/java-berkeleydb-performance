3dd38b8b3e4d3c8428f04b0b781fe504fd6eb8b0
Stephen Just <sajust@ualberta.ca>
2013-11-26 17:33:29 -0700

    Added hash table db class

---
+++ src/HashTableDb.java
@@ -0,0 +1,22 @@
+
+public class HashTableDb implements ISearch {
+
+	@Override
+	public void getByKey(String skey) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getByValue(String value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void retrieveRange(String startKey, String endKey) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
