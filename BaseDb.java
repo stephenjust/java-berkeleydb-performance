5290ccc7fa14503355fce8820ff7a823530a7918
Stephen Just <sajust@cs.ualberta.ca>
2013-11-28 22:51:09 -0700

    Fixing search by value...

--- BaseDb.java
+++ BaseDb.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.Random;
 
 import com.sleepycat.db.Cursor;
@@ -162,16 +163,14 @@ public abstract class BaseDb {
 			DatabaseEntry data = new DatabaseEntry();
 
 			Cursor c = db.openCursor(null, null);
-			c.getFirst(new DatabaseEntry(), new DatabaseEntry(), LockMode.DEFAULT);
-			if (c.getSearchKeyRange(key, data, LockMode.DEFAULT) != //demove this block breaks hashtable
-					OperationStatus.SUCCESS) {
-				System.err.println("No results");
-				return;
+			c.getFirst(key, data, LockMode.DEFAULT);
+			if (compareByteArrays(Arrays.copyOf(data.getData(), data.getSize()), value.getBytes()) == 0) {
+			    addRecordToAnswers(key,data);
+			    recordCount++;
 			}
-			recordCount = 1;
 			while (c.getNext(key, data, LockMode.DEFAULT) == OperationStatus.SUCCESS) {
 				// If the current entry has the value we want, write it to file.
-				if (compareByteArrays(key.getData(), value.getBytes("UTF-8")) == 0) {
+			    if (compareByteArrays(Arrays.copyOf(data.getData(),data.getSize()), value.getBytes("UTF-8")) == 0) {
 					addRecordToAnswers(key, data);
 					recordCount++;
 				}
